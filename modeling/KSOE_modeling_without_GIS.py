581791f99eb83e3a98060f63eea99a4f7421ccc0
SohyunNam <sohyon525@naver.com>
2021-04-07 16:52:14 +0900

    created

--- modeling/KSOE_modeling_without_GIS.py
+++ modeling/KSOE_modeling_without_GIS.py
@@ -3,13 +3,17 @@ import scipy.stats as st
 import numpy as np
 import simpy
 import time
+import datetime
 import functools
-
-
 
 start_running = time.time()
 
 # input data
 data_all = pd.read_excel('../data/Layout_Activity_4_12.xlsx')
-print(data_all)
-print(0)
+
+
+
+test_date = "20140206"
+convert_date = datetime.datetime.strptime(test_date, '%Y%m%d').date()
+
+print(convert_date)
