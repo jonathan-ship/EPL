cd04b8cdba45ffa089450db782b8b88c4455f76f
SohyunNam <sohyon525@naver.com>
2021-07-14 19:15:34 +0900

    KSOE-Layout code, all errors are solved (A0001)

--- KSOE_Layout.py
+++ KSOE_Layout.py
@@ -361,70 +361,70 @@ info_part_moving = info_part_moving.transpose()
 info_part_moving.to_excel(save_path + '/part_distance.xlsx')
 
 
-# tp_time = [i for i in range(math.ceil(processes['Sink'].last_arrival))]
-# tp_unloaded_distance = [0 for _ in range(len(tp_time))]  # 상차 이동 거리
-# tp_loaded_distance = tp_unloaded_distance[:]  # 하차 이동 거리
-# tp_used_time_loaded = tp_unloaded_distance[:]  # 날짜 별 사용 시간 (상차)
-# tp_used_time_unloaded = tp_unloaded_distance[:]  # 날짜 별 사용 시간 (하차)
-#
-# for tp in resource.tp_post_processing.keys():
-#     each_tp = resource.tp_post_processing[tp]
-#     ## loaded
-#     loaded_time = each_tp['loaded']['moving_time']
-#     for i in range(len(loaded_time)):
-#         start_time = round(loaded_time[i][0])
-#         finish_time = round(loaded_time[i][1])
-#         if start_time == finish_time:
-#             tp_loaded_distance[start_time] += each_tp['loaded']['moving_distance'][i]
-#             tp_used_time_loaded[start_time] += loaded_time[i][1] - loaded_time[i][0]
-#         else:  # 시간 비례하여 각 날짜에 포함
-#             day_1 = finish_time - loaded_time[i][0]  # 첫째날에 이동한 총 시간
-#             tp_loaded_distance[start_time] += day_1 * 3 * 1000 * 24
-#             tp_used_time_loaded[start_time] += day_1
-#             day_2 = loaded_time[i][1] - finish_time  # 둘째날에 이동한 총 시간
-#             tp_loaded_distance[finish_time] += day_2 * 3 * 1000 * 24
-#             tp_used_time_loaded[finish_time] += day_2
-#
-#     ## unloaded
-#     unloaded_time = each_tp['unloaded']['moving_time']
-#     for i in range(len(unloaded_time)):
-#         start_time = round(unloaded_time[i][0])
-#         finish_time = round(unloaded_time[i][1])
-#         if start_time == finish_time:
-#             tp_unloaded_distance[start_time] += each_tp['unloaded']['moving_distance'][i]
-#             tp_used_time_unloaded[start_time] += unloaded_time[i][1] - unloaded_time[i][0]
-#         else:  # 시간 비례하여 각 날짜에 포함
-#             day_1 = finish_time - unloaded_time[i][0]  # 첫째날에 이동한 총 시간
-#             tp_unloaded_distance[start_time] += day_1 * 10 * 1000 * 24
-#             tp_used_time_loaded[start_time] += day_1
-#             day_2 = unloaded_time[i][1] - finish_time  # 둘째날에 이동한 총 시간
-#             tp_unloaded_distance[finish_time] += day_2 * 10 * 1000 * 24
-#             tp_used_time_loaded[finish_time] += day_2
-#
-# ## ax1: unloaded time / ax2: loaded_time
-# fig, ax = plt.subplots()
-# tp_used_time_loaded_hour = list(map(lambda x: x*24, tp_used_time_loaded))
-# tp_used_time_unloaded_hour = list(map(lambda x: x*24, tp_used_time_unloaded))
-# unloaded_line_time = ax.plot(tp_time, tp_used_time_unloaded_hour, color="blue", marker=".", label="Unloaded")
-# loaded_bar_time = ax.bar(tp_time, tp_used_time_loaded_hour, color="red", width=5, label="Loaded")
-# ax.set_title("T/P time", fontsize=13, fontweight="bold")
-# ax.set_xlabel("Time")
-# ax.set_ylabel("Used Time [hr]")
-# fig.legend(loc=1, bbox_to_anchor=(1, 1), bbox_transform=ax.transAxes, shadow=True, fancybox=True)
-# filepath = save_path + '/TP_time.png'
-# plt.savefig(filepath, dpi=600, transparent=True)
-# plt.show()
-#
-# fig, ax = plt.subplots()
-# tp_used_distance_loaded_km = list(map(lambda x: x*0.001, tp_loaded_distance))
-# tp_used_distance_unloaded_km = list(map(lambda x: x*0.001, tp_unloaded_distance))
-# unloaded_line_distance = ax.plot(tp_time, tp_used_distance_unloaded_km, color="blue", marker=".", label="Unloaded")
-# loaded_bar_distance = ax.bar(tp_time, tp_used_distance_loaded_km, color="red", width=5, label="Loaded")
-# ax.set_title("T/P Distance", fontsize=13, fontweight="bold")
-# ax.set_xlabel("Time")
-# ax.set_ylabel("Distance [km]")
-# fig.legend(loc=1, bbox_to_anchor=(1, 1), bbox_transform=ax.transAxes, shadow=True, fancybox=True)
-# filepath = save_path + '/TP_distance.png'
-# plt.savefig(filepath, dpi=600, transparent=True)
-# plt.show()
+tp_time = [i for i in range(math.ceil(processes['Sink'].last_arrival) + 1)]
+tp_unloaded_distance = [0 for _ in range(len(tp_time))]  # 상차 이동 거리
+tp_loaded_distance = tp_unloaded_distance[:]  # 하차 이동 거리
+tp_used_time_loaded = tp_unloaded_distance[:]  # 날짜 별 사용 시간 (상차)
+tp_used_time_unloaded = tp_unloaded_distance[:]  # 날짜 별 사용 시간 (하차)
+
+for tp in resource.tp_post_processing.keys():
+    each_tp = resource.tp_post_processing[tp]
+    ## loaded
+    loaded_time = each_tp['loaded']['moving_time']
+    for i in range(len(loaded_time)):
+        start_time = round(loaded_time[i][0])
+        finish_time = round(loaded_time[i][1])
+        if start_time == finish_time:
+            tp_loaded_distance[start_time] += each_tp['loaded']['moving_distance'][i]
+            tp_used_time_loaded[start_time] += loaded_time[i][1] - loaded_time[i][0]
+        else:  # 시간 비례하여 각 날짜에 포함
+            day_1 = finish_time - loaded_time[i][0]  # 첫째날에 이동한 총 시간
+            tp_loaded_distance[start_time] += day_1 * 3 * 1000 * 24
+            tp_used_time_loaded[start_time] += day_1
+            day_2 = loaded_time[i][1] - finish_time  # 둘째날에 이동한 총 시간
+            tp_loaded_distance[finish_time] += day_2 * 3 * 1000 * 24
+            tp_used_time_loaded[finish_time] += day_2
+
+    ## unloaded
+    unloaded_time = each_tp['unloaded']['moving_time']
+    for i in range(len(unloaded_time)):
+        start_time = round(unloaded_time[i][0])
+        finish_time = round(unloaded_time[i][1])
+        if start_time == finish_time:
+            tp_unloaded_distance[start_time] += each_tp['unloaded']['moving_distance'][i]
+            tp_used_time_unloaded[start_time] += unloaded_time[i][1] - unloaded_time[i][0]
+        else:  # 시간 비례하여 각 날짜에 포함
+            day_1 = finish_time - unloaded_time[i][0]  # 첫째날에 이동한 총 시간
+            tp_unloaded_distance[start_time] += day_1 * 10 * 1000 * 24
+            tp_used_time_loaded[start_time] += day_1
+            day_2 = unloaded_time[i][1] - finish_time  # 둘째날에 이동한 총 시간
+            tp_unloaded_distance[finish_time] += day_2 * 10 * 1000 * 24
+            tp_used_time_loaded[finish_time] += day_2
+
+## ax1: unloaded time / ax2: loaded_time
+fig, ax = plt.subplots()
+tp_used_time_loaded_hour = list(map(lambda x: x*24, tp_used_time_loaded))
+tp_used_time_unloaded_hour = list(map(lambda x: x*24, tp_used_time_unloaded))
+unloaded_line_time = ax.plot(tp_time, tp_used_time_unloaded_hour, color="blue", marker=".", label="Unloaded")
+loaded_bar_time = ax.bar(tp_time, tp_used_time_loaded_hour, color="red", width=5, label="Loaded")
+ax.set_title("T/P time", fontsize=13, fontweight="bold")
+ax.set_xlabel("Time")
+ax.set_ylabel("Used Time [hr]")
+fig.legend(loc=1, bbox_to_anchor=(1, 1), bbox_transform=ax.transAxes, shadow=True, fancybox=True)
+filepath = save_path + '/TP_time.png'
+plt.savefig(filepath, dpi=600, transparent=True)
+plt.show()
+
+fig, ax = plt.subplots()
+tp_used_distance_loaded_km = list(map(lambda x: x*0.001, tp_loaded_distance))
+tp_used_distance_unloaded_km = list(map(lambda x: x*0.001, tp_unloaded_distance))
+unloaded_line_distance = ax.plot(tp_time, tp_used_distance_unloaded_km, color="blue", marker=".", label="Unloaded")
+loaded_bar_distance = ax.bar(tp_time, tp_used_distance_loaded_km, color="red", width=5, label="Loaded")
+ax.set_title("T/P Distance", fontsize=13, fontweight="bold")
+ax.set_xlabel("Time")
+ax.set_ylabel("Distance [km]")
+fig.legend(loc=1, bbox_to_anchor=(1, 1), bbox_transform=ax.transAxes, shadow=True, fancybox=True)
+filepath = save_path + '/TP_distance.png'
+plt.savefig(filepath, dpi=600, transparent=True)
+plt.show()
 
