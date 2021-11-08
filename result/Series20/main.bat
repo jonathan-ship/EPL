183063ef4c6ee8f4fa66e93463612b7b10a6ad7f
SohyunNam <sohyon525@naver.com>
2021-11-08 12:12:05 +0900

    Add Transporter information and redetermine source location

---
+++ result/Series20/main.bat
@@ -0,0 +1,6 @@
+call conda env list 
+call conda activate env_sim 
+cd C:/Users/sohyon/PycharmProjects/Simulation_Module 
+call python main.py C:/Users/sohyon/PycharmProjects/Simulation_Module/data/input_data_tp.json 
+
+pause
---
+++ result/Series20/post_processing.bat
@@ -0,0 +1,6 @@
+call conda env list 
+call conda activate env_sim 
+cd C:/Users/sohyon/PycharmProjects/Simulation_Module 
+call python Postprocessing.py ./result/Series20/result_path.json 
+
+pause
